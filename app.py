afaa34614a4bd14423d27ee62587299ba8a6e442
MD.Abdullah Ibne Aziz <22203246@iubat.edu>
2025-05-04 10:07:59 +0600

    updated

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import joblib
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__, static_folder='.', template_folder='.')
+app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('Model/best_model.joblib')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/script.js')
 def serve_script():
